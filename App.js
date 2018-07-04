37573d2f898ad138761e652448ce8847d1435d7d
ExoMemphiz <ExoMemphiz@gmail.com>
2018-07-04 11:04:33 +0200

    Update #1

--- App.js
+++ App.js
@@ -4,49 +4,63 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Platform,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 import CircleSlider from './CircleSlider'
 
 export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-      <View style={{flex: 2}} />
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <CircleSlider
-          value={90}
-          textColor={"white"}
-          xCenter={50}
-          yCenter={50}
-        />
-      </View>
-      </View>
-    );
-  }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={{flex: 0}}>
+                </View>
+                <View style={{flex: 1}}>
+                    <CircleSlider
+                        value={90}
+                        textColor={"white"}
+                        startGradient='#01fffc'
+                        endGradient='#a200ff'
+                        startCoord={180}
+                    />
+                </View>
+            </View>
+        );
+    }
 }
 
+/*
+
+<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                    <CircleSlider
+                        value={90}
+                        textColor={"white"}
+                    />
+                </View>
+
+ */
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'gray',
-  },
-  welcome: {
-    fontSize: 30,
-    textAlign: 'center',
-    marginTop: 50,
-    color: 'white'
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'gray',
+    },
+    welcome: {
+        fontSize: 30,
+        textAlign: 'center',
+        marginTop: 50,
+        color: 'white'
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
--- CircleSlider.js
+++ CircleSlider.js
@@ -1,134 +1,159 @@
-import React, { Component } from "react";
-import { PanResponder, View, Dimensions } from "react-native";
+import React, {Component} from "react";
+import {PanResponder, View, Dimensions} from "react-native";
 import Svg, {
-	Path,
-	Circle,
-	G,
-	Text,
-	Defs,
-	LinearGradient,
-	Stop
+    Path,
+    Circle,
+    G,
+    Text,
+    Defs,
+    LinearGradient,
+    Stop
 } from "react-native-svg";
 
 export default class CircleSlider extends Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.state = {
-			angle: this.props.value
-		};
-	}
+        this.state = {
+            angle: this.props.value,
+            xCenter: 0,
+            yCenter: 0
+        };
+    }
 
-	componentWillMount() {
-		this._panResponder = PanResponder.create({
-			onStartShouldSetPanResponder: (e, gs) => true,
-			onStartShouldSetPanResponderCapture: (e, gs) => true,
-			onMoveShouldSetPanResponder: (e, gs) => true,
-			onMoveShouldSetPanResponderCapture: (e, gs) => true,
-			onPanResponderMove: (e, gs) => {
-				let xOrigin =
-					this.props.xCenter - (this.props.dialRadius + this.props.btnRadius);
-				let yOrigin =
-					this.props.yCenter - (this.props.dialRadius + this.props.btnRadius);
-				let a = this.cartesianToPolar(gs.moveX - xOrigin, gs.moveY - yOrigin);
-				this.setState({ angle: a });
-			}
-		});
-	}
+    componentWillMount() {
+        this._panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: (e, gs) => true,
+            onStartShouldSetPanResponderCapture: (e, gs) => true,
+            onMoveShouldSetPanResponder: (e, gs) => true,
+            onMoveShouldSetPanResponderCapture: (e, gs) => true,
+            onPanResponderMove: (e, gs) => {
+                let xOrigin = this.state.xCenter - (this.props.dialRadius + this.props.btnRadius);
+                let yOrigin = this.state.yCenter - (this.props.dialRadius + this.props.btnRadius);
+                let a = this.cartesianToPolar(gs.moveX - xOrigin, gs.moveY - yOrigin);
+                this.setState({angle: a});
+            }
+        });
+    }
 
-	polarToCartesian(angle) {
-		let r = this.props.dialRadius;
-		let hC = this.props.dialRadius + this.props.btnRadius;
-		let a = ((angle - 90) * Math.PI) / 180.0;
+    polarToCartesian(angle) {
+        let r = this.props.dialRadius;
+        let hC = this.props.dialRadius + this.props.btnRadius;
+        let a = ((angle - 90) * Math.PI) / 180.0;
 
-		let x = hC + r * Math.cos(a);
-		let y = hC + r * Math.sin(a);
-		return { x, y };
-	}
+        let x = hC + r * Math.cos(a);
+        let y = hC + r * Math.sin(a);
+        return {x, y};
+    }
 
-	cartesianToPolar(x, y) {
-		let hC = this.props.dialRadius + this.props.btnRadius;
+    cartesianToPolar(x, y) {
+        let hC = this.props.dialRadius + this.props.btnRadius;
 
-		if (x === 0) {
-			return y > hC ? 0 : 180;
-		} else if (y === 0) {
-			return x > hC ? 90 : 270;
-		} else {
-			return (
-				Math.round((Math.atan((y - hC) / (x - hC)) * 180) / Math.PI) +
-				(x > hC ? 90 : 270)
-			);
-		}
-	}
+        if (x === 0) {
+            return y > hC ? 0 : 180;
+        } else if (y === 0) {
+            return x > hC ? 90 : 270;
+        } else {
+            return (
+                Math.round((Math.atan((y - hC) / (x - hC)) * 180) / Math.PI) +
+                (x > hC ? 90 : 270)
+            );
+        }
+    }
 
-	render() {
-		let width = (this.props.dialRadius + this.props.btnRadius) * 2;
-		let bR = this.props.btnRadius;
-		let dR = this.props.dialRadius;
-		let startCoord = this.polarToCartesian(0);
-		let endCoord = this.polarToCartesian(this.state.angle);
+    handleMeasure = (ox, oy, width, height, px, py) => {
+        this.setState({
+            xCenter: px + (this.props.dialRadius + this.props.btnRadius),
+            yCenter: py + (this.props.dialRadius + this.props.btnRadius)
+        });
+    }
 
-		return (
-			<Svg ref="circleslider" width={width} height={width}>
-				<Defs>
-					<LinearGradient id="gradient1" x1="0%" y1="0%" x2="100%" y2="0%">
-						<Stop offset="0%" stopColor={this.props.startGradient} />
-						<Stop offset="100%" stopColor={this.props.endGradient} />
-					</LinearGradient>
-				</Defs>
-				<Circle
-					r={dR}
-					cx={width / 2}
-					cy={width / 2}
-					stroke={this.props.backgroundColor}
-					strokeWidth={25}
-					fill="none"
-				/>
-				<Text
-					x={width / 2}
-					y={width / 2}
-					fontSize={this.props.textSize}
-					fill={this.props.textColor}
-					textAnchor="middle"
-				>
-					{this.props.showValue &&
-						this.props.onValueChange(this.state.angle) + ""}
-				</Text>
-				<Path
-					stroke={"url(#gradient1)"}
-					strokeWidth={this.props.dialWidth}
-					fill="none"
-					d={`M${startCoord.x} ${startCoord.y} A ${dR} ${dR} 0 ${
-						this.state.angle > 180 ? 1 : 0
-					} 1 ${endCoord.x} ${endCoord.y}`}
-				/>
+    doStuff = () => {
+        this.refs.circleslider.measure(this.handleMeasure);
+    }
 
-				<G x={endCoord.x - bR} y={endCoord.y - bR}>
-					<Circle
-						r={bR}
-						cx={bR}
-						cy={bR}
-						fill={"white"}
-						{...this._panResponder.panHandlers}
-					/>
-				</G>
-			</Svg>
-		);
-	}
+    render() {
+
+        let width = (this.props.dialRadius + this.props.btnRadius) * 2;
+        let bR = this.props.btnRadius;
+        let dR = this.props.dialRadius;
+        let startCoord = this.polarToCartesian(this.props.startCoord);
+        let endCoord = this.polarToCartesian(this.state.angle);
+
+        return (
+            <View style={{flex: 1}}>
+                <Svg onLayout={this.doStuff} ref="circleslider" width={width} height={width}>
+                    <Defs>
+                        <LinearGradient id="gradient1" x1="0%" y1="0%" x2="100%" y2="0%">
+                            <Stop offset="0%" stopColor={this.props.startGradient}/>
+                            <Stop offset="100%" stopColor={this.props.endGradient}/>
+                        </LinearGradient>
+                    </Defs>
+                    <Circle
+                        r={dR}
+                        cx={width / 2}
+                        cy={width / 2}
+                        stroke={this.props.backgroundColor}
+                        strokeWidth={25}
+                        fill="none"
+                    />
+                    <Text
+                        x={width / 2}
+                        y={width / 2 + 50}
+                        fontSize={14}
+                        fill={this.props.textColor}
+                        textAnchor="middle">
+                        {`Start Coord X: ${startCoord.x}, Y: ${startCoord.y}`}
+                    </Text>
+                    <Text
+                        x={width / 2}
+                        y={width / 2}
+                        fontSize={this.props.textSize}
+                        fill={this.props.textColor}
+                        textAnchor="middle"
+                    >
+                        {this.props.showValue &&
+                        this.props.onValueChange(this.state.angle) + ""}
+                    </Text>
+                    <Path
+                        stroke={"url(#gradient1)"}
+                        strokeWidth={this.props.dialWidth}
+                        fill="none"
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        d={`M${startCoord.x} ${startCoord.y} A ${dR} ${dR} 1 ${(this.props.startCoord + 180) % 360 > this.state.angle ? 1 : 0} 0 ${endCoord.x} ${endCoord.y}`}
+                    />
+
+                    <G x={endCoord.x - bR} y={endCoord.y - bR}>
+                        <Circle
+                            r={bR}
+                            cx={bR}
+                            cy={bR}
+                            fill={"yellow"}
+                            {...this._panResponder.panHandlers}
+                        />
+                    </G>
+
+
+                </Svg>
+            </View>
+        );
+    }
 }
 
 CircleSlider.defaultProps = {
-	btnRadius: 13,
-	dialRadius: 130,
-	dialWidth: 25,
-	textColor: "white",
-	textSize: 50,
-	value: 0,
-	xCenter: Dimensions.get("window").width / 2,
-	yCenter: Dimensions.get("window").height / 2,
-  showValue: true,
-  startGradient: '#12D8FA',
-  endGradient: '#A6FFCB',
-  backgroundColor: 'white',
-	onValueChange: x => x
+    btnRadius: 13,
+    dialRadius: 130,
+    dialWidth: 25,
+    textColor: "white",
+    textSize: 50,
+    value: 0,
+    xCenter: Dimensions.get("window").width / 2,
+    yCenter: Dimensions.get("window").height / 2,
+    showValue: true,
+    startGradient: '#12D8FA',
+    endGradient: '#A6FFCB',
+    backgroundColor: 'white',
+    startCoord: 0,
+    onValueChange: x => x
 };
